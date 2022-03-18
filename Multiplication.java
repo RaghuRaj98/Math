1dadd921437f58bdea57e8627e9fb27f1fdce60a
RaghuRaj98 <raghavendraraj1998@gmail.com>
2022-03-18 19:20:52 +0530

    fifth commit

--- Multiplication.java
+++ Multiplication.java
@@ -3,4 +3,11 @@ package com.git;
 public class Multiplication {
 		
 	
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+		
+		System.out.println(("Multiplication")+(a*b));
+	}
 }
